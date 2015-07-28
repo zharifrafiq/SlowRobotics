91fd4345842c29dad888177d787c275416bb3504
Gwyllim Jahn <gwyllo@gmail.com>
2015-07-28 21:00:41 +1000

    added scanner classes

---
+++ SlowRobotics/src/pointCloudTools/Scanner.java
@@ -0,0 +1,8 @@
+package pointCloudTools;
+
+import toxi.geom.AABB;
+
+public interface Scanner {
+
+	public float[][] copyAABB(AABB box);
+}
---
+++ SlowRobotics/src/pointCloudTools/StructureScanner.java
@@ -0,0 +1,17 @@
+package pointCloudTools;
+
+import toxi.geom.AABB;
+
+public class StructureScanner implements Scanner{
+	
+	public StructureScanner(){
+		//TODO implement structureIO code
+	}
+	
+	@Override
+	public float[][] copyAABB(AABB box) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
